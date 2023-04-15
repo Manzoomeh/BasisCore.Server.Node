f90734ba4023c352f79a966294f1eaf2b3247c66
Qamsari <44198226+Qamsari@users.noreply.github.com>
2023-04-15 16:54:01 +0330

    add file streamer to http2

--- endPoint/h2HttpHostEndPoint.js
+++ endPoint/h2HttpHostEndPoint.js
@@ -1,14 +1,15 @@
 import http2 from "http2";
+import busboy from "busboy";
 import { StatusCodes } from "http-status-codes";
-import SecureHttpHostEndPoint from "./SecureHttpHostEndPoint.js";
+import SecureHttpHostEndPoint from "./secureHttpHostEndPoint.js";
 import HostService from "../services/hostService.js";
-import SecureContextOptions from "tls";
+import BinaryContent from "../fileStreamer/Models/BinaryContent.js";
 
 export default class H2HttpHostEndPoint extends SecureHttpHostEndPoint {
   /** @type {HostService} */
   #service;
 
-  /** @type {SecureContextOptions} */
+  /** @type {import("tls").SecureContextOptions} */
   #options;
 
   /**
@@ -65,16 +66,11 @@ export default class H2HttpHostEndPoint extends SecureHttpHostEndPoint {
         const fileContents = [];
         /**@type {NodeJS.Dict<string>} */
         const formFields = {};
-        stream.on("error", (ex) => {
-          if (ex.code != "ERR_STREAM_WRITE_AFTER_END") {
-            console.error("HTTP/2 server stream error", ex);
-          }
-          stream.destroy(ex);
-        });
-        try {
+        const method = headers[":method"];
+        const createCmsAndCreateResponseAsync = async () => {
           cms = await this._createCmsObjectAsync(
             headers[":path"],
-            headers[":method"],
+            method,
             headers,
             formFields,
             fileContents,
@@ -85,6 +81,49 @@ export default class H2HttpHostEndPoint extends SecureHttpHostEndPoint {
           headerList[":status"] = code;
           stream.respond(headerList);
           stream.end(body);
+        };
+        stream.on("error", (ex) => {
+          if (ex.code != "ERR_STREAM_WRITE_AFTER_END") {
+            console.error("HTTP/2 server stream error", ex);
+          }
+          stream.destroy(ex);
+        });
+        try {
+          if (method === "POST") {
+            /**@type {Array<BinaryContent>}*/
+            const bb = busboy({ headers: headers });
+            bb.on("file", (name, file, info) => {
+              const ContentParts = [];
+              file.on("data", (x) => ContentParts.push(x));
+              file.on("end", async () => {
+                const content = new BinaryContent();
+                content.mime = info.mimeType.toLowerCase();
+                content.name = info.filename;
+                content.payload = Buffer.concat(ContentParts);
+                fileContents.push(content);
+              });
+            });
+            bb.on("field", (name, val, info) => {
+              formFields[name] = val;
+            });
+            bb.on("close", createCmsAndCreateResponseAsync);
+            stream.pipe(bb);
+          } else {
+            createCmsAndCreateResponseAsync();
+          }
+          // cms = await this._createCmsObjectAsync(
+          //   headers[":path"],
+          //   headers[":method"],
+          //   headers,
+          //   formFields,
+          //   fileContents,
+          //   stream.session.socket
+          // );
+          // const result = await this.#service.processAsync(cms);
+          // const [code, headerList, body] = await result.getResultAsync();
+          // headerList[":status"] = code;
+          // stream.respond(headerList);
+          // stream.end(body);
         } catch (ex) {
           if (ex.code != "ERR_HTTP2_INVALID_STREAM") {
             console.error("HTTP/2 server error", ex);
