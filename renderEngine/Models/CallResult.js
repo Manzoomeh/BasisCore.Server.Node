8355b44e7e8b09ddbe63e2c2da5a558ad154daab
Qamsari <44198226+Qamsari@users.noreply.github.com>
2023-11-26 23:01:10 +0330

    step 1

--- renderEngine/Models/CallResult.js
+++ renderEngine/Models/CallResult.js
@@ -3,4 +3,8 @@ import ICommandResult from "./ICommandResult.js";
 export default class CallResult extends ICommandResult {
   /** @type {Array<CommandBase>} */
   commands;
+
+  constructor() {
+    super();
+  }
 }
