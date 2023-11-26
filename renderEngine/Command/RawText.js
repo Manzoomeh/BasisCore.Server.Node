849c2fb6e8d7fa2dbeb7dd452d9f1d1878dd1a62
Qamsari <44198226+Qamsari@users.noreply.github.com>
2023-11-26 23:48:05 +0330

    Adding RawText command #39

---
+++ renderEngine/Command/RawText.js
@@ -0,0 +1,24 @@
+import TokenUtil from "../Token/TokenUtil.js";
+import StringResult from "../Models/StringResult.js";
+import CommandBase from "./CommandBase.js";
+
+export default class RawText extends CommandBase {
+  /**@type {IToken} */
+  content;
+  /**
+   * @param {object} rawTextIl
+   */
+  constructor(rawTextIl) {
+    super(rawTextIl);
+    this.content = TokenUtil.getFiled(rawTextIl, "content");
+  }
+
+  /**
+   * @param {IContext} context
+   * @returns {Promise<ICommandResult>}
+   */
+  async executeAsync(context) {
+    const content = this.content.getValueAsync(context);
+    return new StringResult(content);
+  }
+}
