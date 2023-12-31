7c66d5eeaea01561daf8285d1ad3b5e3f28f06d9
ali bazregar <abazregar9@gmail.com>
2023-12-31 16:34:45 +0330

    add v1

--- Models/Connection/IMongoSettingData.js
+++ Models/Connection/IMongoSettingData.js
@@ -1,4 +1,12 @@
 export default class IMongoSettingData {
   /** @type {string} */
+  dataBase;
+  /** @type {string}*/
   endpoint;
+  /** @type {string}*/
+  collection;
+  /** @type {string}*/
+  method;
+  /**@type {object} */
+  query;
 }
--- Models/Connection/MongoConnectionInfo.js
+++ Models/Connection/MongoConnectionInfo.js
@@ -1,23 +1,25 @@
 import { MongoClient } from "mongodb";
-import net from "net";
 import ConnectionInfo from "./ConnectionInfo.js";
 import DataSourceCollection from "../../renderEngine/Source/DataSourceCollection.js";
 import CancellationToken from "../../renderEngine/Cancellation/CancellationToken.js";
 import Request from "./../request.js";
 import IMongoSettingData from "./IMongoSettingData.js";
-import MongoMessage from "../../Mongo/MongoMessage.js";
-
+import BasisCoreException from "../Exceptions/BasisCoreException.js";
 export default class MongoConnectionInfo extends ConnectionInfo {
   /** @type {IMongoSettingData} */
   settings;
+  /**@param {MongoClient} client */
+  client;
 
   /**
    * @param {string} name
    * @param {IMongoSettingData} settings
+   * @param {MongoClient} client
    */
   constructor(name, settings) {
     super(name);
     this.settings = settings;
+    this.client = new MongoClient(this.settings.endpoint);
   }
 
   /**
@@ -27,10 +29,18 @@ export default class MongoConnectionInfo extends ConnectionInfo {
    */
   async loadDataAsync(parameters, cancellationToken) {
     try {
-      const client = new MongoClient(this.settings.endpoint);
-      await client.connect();
-    } finally {
-      await client.close();
+      await this.client.connect();
+      const db = this.client.db(this.settings.dataBase);
+      const collection = db.collection(this.settings.collection);
+      if (typeof collection[this.settings.method] != "function") {
+        throw new BasisCoreException("invalid method");
+      }
+      const result = await collection[this.settings.method](this.settings.query)
+      return new DataSourceCollection(await result.toArray());
+    } catch (err) {
+      throw err;
+   } finally {
+    await this.client.close();
     }
   }
 
@@ -40,41 +50,7 @@ export default class MongoConnectionInfo extends ConnectionInfo {
    * @returns {Promise<IDataSource>}
    */
   async getRoutingDataAsync(request, cancellationToken) {
-    const task = new Promise((resolve, reject) => {
-      const buffer = [];
-      const client = new net.Socket()
-        .on("data", (data) => buffer.push(data))
-        .on("close", function () {
-          const data = Buffer.concat(buffer);
-          try {
-            const msg = MongoMessage.createFromBuffer(data);
-            resolve(JSON.parse(msg.payload));
-          } catch (e) {
-            reject(e);
-          }
-        })
-        .on("error", (e) => {
-          console.error(e);
-          reject(e);
-        })
-        .connect(this.#port, this.#ip, () => {
-          const msg = MongoMessage.createAdHocMessageFromObject({
-            cms: request,
-          });
-          msg.writeTo(client);
-        });
-    });
-    const result = await task;
-    //TODO: must edit in Mongo side
-    if (result.cms.webserver) {
-      result.webserver = result.cms.webserver;
-      delete result.cms.webserver;
-    }
-    if (result.cms.http) {
-      result.http = result.cms.http;
-      delete result.cms.http;
-    }
-    return result;
+    throw new Error("get routing data is not supported!");
   }
 
   /**
@@ -82,34 +58,14 @@ export default class MongoConnectionInfo extends ConnectionInfo {
    * @param {BinaryContent[]} fileContents
    * @returns {Promise<Response>}
    */
-  async processAsync(request, fileContents) {
-    await this._processUploadAsync(fileContents, request);
-    /** @type {Promise<Request>} */
-    const task = new Promise((resolve, reject) => {
-      const buffer = [];
-      const client = new net.Socket()
-        .on("data", (data) => buffer.push(data))
-        .on("close", function () {
-          const data = Buffer.concat(buffer);
-          try {
-            const msg = MongoMessage.createFromBuffer(data);
-            resolve(JSON.parse(msg.payload));
-          } catch (e) {
-            reject(e);
-          }
-        })
-        .on("error", (e) => {
-          console.error(e);
-          reject(e);
-        })
-        .connect(this.#port, this.#ip, () => {
-          const msg = MongoMessage.createAdHocMessageFromObject({
-            cms: request,
-          });
-          msg.writeTo(client);
-        });
-    });
-    const result = await task;
-    return this._createResponse(result);
+  async testConnectionAsync() {
+    try{
+      await this.client.connect()
+      return true
+    }catch(err){
+      return false
+    }finally{
+      await this.client.close()
+    }
   }
 }
