c3050621303b5d49b0b91e42289a23738202b3ae
ali bazregar <abazregar9@gmail.com>
2024-01-16 12:06:25 +0330

    remove empty line

--- test/command/CommandUtil.js
+++ test/command/CommandUtil.js
@@ -16,7 +16,6 @@ export default class CommandUtil {
    * @returns {CommandBase}
    */
   static createCommand(commandIl) {
-
     //TODO:must be better with dic of ctor
     /** @type {CommandBase?} */
     let retVal = null;
