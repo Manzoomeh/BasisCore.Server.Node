c9f703a13a2f468552dad2ee008c3802ce33b5ed
Qamsari <44198226+Qamsari@users.noreply.github.com>
2024-02-20 17:40:58 +0330

    feat: Adding UnknownCommand #84

--- test/command/CommandUtil.js
+++ test/command/CommandUtil.js
@@ -9,6 +9,7 @@ import TreeCommand from "../../renderEngine/Command/TreeCommand.js";
 import ViewCommand from "../../renderEngine/Command/ViewCommand.js";
 import ListCommand from "../../renderEngine/Command/ListCommand.js";
 import CallCommand from "../../renderEngine/Command/Collection/CallCommand.js";
+import UnknownCommand from "../../renderEngine/Command/UnknownCommand.js";
 
 export default class CommandUtil {
   /**
@@ -19,48 +20,52 @@ export default class CommandUtil {
     //TODO:must be better with dic of ctor
     /** @type {CommandBase?} */
     let retVal = null;
-      switch (commandIl.$type.toLowerCase()) {
-        case "rawtext": {
-          retVal = new RawText(commandIl);
-          break;
-        }
-        case "print": {
-          retVal = new PrintCommand(commandIl);
-          break;
-        }
-        case "group": {
-          retVal = new GroupCommand(commandIl);
-          break;
-        }
-        case "dbsource": {
-          retVal = new DbSource(commandIl);
-          break;
-        }
-        case "inlinesource": {
-          retVal = new InlineSourceCommand(commandIl);
-          break;
-        }
-        case "tree": {
-          retVal = new TreeCommand(commandIl);
-          break;
-        }
-        case "view": {
-          retVal = new ViewCommand(commandIl);
-          break;
-        }
-        case "list": {
-          retVal = new ListCommand(commandIl);
-          break;
-        }
-        case "call": {
-          retVal = new CallCommand(commandIl);
-          break;
-        }
-        case "api": {
-          retVal = new ApiCommand(commandIl);
-          break;
-        }
+    switch (commandIl.$type.toLowerCase()) {
+      case "rawtext": {
+        retVal = new RawText(commandIl);
+        break;
       }
+      case "print": {
+        retVal = new PrintCommand(commandIl);
+        break;
+      }
+      case "group": {
+        retVal = new GroupCommand(commandIl);
+        break;
+      }
+      case "dbsource": {
+        retVal = new DbSource(commandIl);
+        break;
+      }
+      case "inlinesource": {
+        retVal = new InlineSourceCommand(commandIl);
+        break;
+      }
+      case "tree": {
+        retVal = new TreeCommand(commandIl);
+        break;
+      }
+      case "view": {
+        retVal = new ViewCommand(commandIl);
+        break;
+      }
+      case "list": {
+        retVal = new ListCommand(commandIl);
+        break;
+      }
+      case "call": {
+        retVal = new CallCommand(commandIl);
+        break;
+      }
+      case "api": {
+        retVal = new ApiCommand(commandIl);
+        break;
+      }
+      default: {
+        retVal = new UnknownCommand(commandIl);
+        break;
+      }
+    }
     return retVal;
   }
 }
