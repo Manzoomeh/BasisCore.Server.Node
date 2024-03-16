4e70858bc6c49962885bec740f9fa87ba6231380
Qamsari <44198226+Qamsari@users.noreply.github.com>
2024-03-16 15:03:12 +0330

    Fix recordset is undefined in execute() method result #98

--- renderEngine/Source/SourceUtil.js
+++ renderEngine/Source/SourceUtil.js
@@ -1,6 +1,7 @@
 import alasql from "alasql";
 import IDataSource from "./IDataSource.js";
 import IContext from "../Context/IContext.js";
+import StringUtil from "../Token/StringUtil.js";
 
 export default class SourceUtil {
   /**
