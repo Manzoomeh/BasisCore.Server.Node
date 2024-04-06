ea4e42ce7a0cbadfc11e444768885e562a6a781e
alibazregar <abazregar9@gmail.com>
2024-04-06 10:17:40 +0330

    fix the parraler mode

--- renderEngine/Command/Source/BaseClasses/MemberCollection.js
+++ renderEngine/Command/Source/BaseClasses/MemberCollection.js
@@ -33,8 +33,9 @@ export default class MemberCollection {
    * @returns {Promise<void>}
    */
   async addHtmlElementAsync(ownerTag, context) {
-    for (const item of this.items) {
-      ownerTag.addChild(await item.createHtmlElementAsync(context));
-    }
+    const elements = await Promise.all(
+      this.items.map((x) => x.createHtmlElementAsync(context))
+    );
+    elements.forEach((x) => ownerTag.addChild(x));
   }
 }
